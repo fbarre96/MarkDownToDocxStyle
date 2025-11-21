b2a9f713816f37db7e42b5f0cbd0e65eea39e3a2
fbarre <fbarre96@hotmail.fr>
2025-11-21 13:44:44 +0100

    update setup

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+# Read requirements from requirements.txt
+with open(HERE / "requirements.txt", "r", encoding="utf-8") as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 setup(
     name='markdowntodocx',
     packages=find_packages(include=['markdowntodocx']),
-    version='0.1.7.5',
+    version='0.1.8.1',
     url="https://github.com/fbarre96/MarkDownToDocxStyle",
     description='Convert markdown inside Docx to docx styles',
     long_description=README,
     long_description_content_type="text/markdown",
     author='Fabien Barre',
     license='MIT',
-    setup_requires=['python-docx>=1.1.2',"requests"],
+    install_requires=requirements,
 )
